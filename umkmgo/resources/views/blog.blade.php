93dda0eeffea394c34c724dc4a3a8f37c367d9b7
Alifiannisa Alyahasna Wighneswara <alifialyaa@gmail.com>
2018-11-27 09:26:54 +0700

    Add files via upload

---
+++ umkmgo/resources/views/blog.blade.php
@@ -0,0 +1,56 @@
+@extends('template.navbar')
+
+@section('title')
+UMKMgo - Blog  
+@endsection
+
+@section('css')
+    <style>
+        .bg{
+          background: linear-gradient(rgba(0,0,0,.7), rgba(0,0,0,.7)), url('bg2.jpg');
+          background-position: center;
+          background-size: cover;
+        }
+    
+    </style>
+@endsection
+
+@section('body')
+    class="bg bg-overlay"
+@endsection
+
+
+@section('content')
+<main role="main">
+  <section class="jumbotron text-center container">
+    <div class="container">
+      <h1 class="jumbotron-heading">Tentang UMKMgo</h1>
+    </div>
+  </section>
+
+  <div class="album py-5 bg-alifi container">
+ <body color= "white">
+ 	<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+<p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
+
+<p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, fergusso!</p>
+
+	<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+<p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
+
+<p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, fergusso!</p>
+
+	<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+<p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
+
+<p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, fergusso!</p>
+ </body>
+</div>
+
+</main>
+
+@endsection
+
